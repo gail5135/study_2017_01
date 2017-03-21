509b006e0fefee20a69db2c13c819b8046fd0f1f
gail5135 <gail5135@gmail.com>
2017-03-22 02:59:06 +0900

    Complete
    QIndex Complete

---
+++ Member/HaeChang12/MarchWeek3/Main.java
@@ -0,0 +1,8 @@
+package com.company;
+
+public class Main {
+    public static void main(String[] args) {
+        // QIndex 실행
+        new QIndex();
+    }
+}
---
+++ Member/HaeChang12/MarchWeek3/QIndex.java
@@ -0,0 +1,58 @@
+package com.company;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.Scanner;
+
+/**
+ * Created by gail5135 on 2017. 3. 21..
+ */
+public class QIndex {
+    private int numOfThesis;
+    private int[] quote;
+
+    private Scanner scan = new Scanner(System.in);
+
+    public QIndex() {
+        // 논문의 갯수를 입력 받음
+        numOfThesis = Integer.parseInt(scan.nextLine());
+
+        // 논문들을 담을 quote 생성
+        quote = new int[numOfThesis];
+
+        // 각각의 인용된 횟수를 입력하고 quote에 넣음
+        inputNum();
+
+        // 인용된 횟수를 오름차순으로 정렬
+        Arrays.sort(quote);
+
+        // q-index를 구하고 화면에 출력
+        calcuQ(quote);
+    }
+
+    // 각각 인용된 횟수를 입력 받고 quote에 담는 메소드
+    public void inputNum(){
+        for(int a = 0; a < quote.length; a++) {
+            quote[a] = scan.nextInt();
+            // quote[a] = Integer.parseInt(scan.nextLine());
+        }
+    }
+
+    // q-index를 체크하는 메소드
+    public void calcuQ(int[] Q){
+        for(int i = 0; i < Q.length; i++){
+            if(Q[i] == (Q.length-i)) {
+                System.out.println(Q[i]);
+                return;
+            }
+        }
+        System.out.println(0);
+    }
+
+    // Array를 출력하는 메소드
+    public void showArray(int[] array){
+        for(int a = 0; a < array.length; a++){
+            System.out.print(array[a] + "\t");
+        }
+    }
+}
